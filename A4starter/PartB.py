d02cd50df7dd32f6f62dc4a235bc12c4ebac164c
Carlos Alves [Kuro Suzume] <carlos.ca.eduardo@gmail.com>
2023-03-06 15:40:32 -0300

    changes in partb.py

--- A4starter/PartB.py
+++ A4starter/PartB.py
@@ -6,15 +6,18 @@ import numpy as np
 class TemperatureHelper:
     def __init__(self,  data_file):
         self.data = {}
-        with open(data_file, 'r') as file:
+        with open(data_file, "r") as file:
+            next(file)
             for line in file:
+
+                line.strip("\n")
+                # split the line
+                lineSplit = line.split(",")
                 # for the first line in the file
-                if (line.split()[1] == "CITY"):
-                    pass
-                else:
-                    city, date, temperature = line.strip().split(",")
-                    year, month, day = map(float, date.split('-'))
-                    self.data[city][year][month][day] = float(temperature)
+
+                city, date, temperature = lineSplit
+                year, month, day = map(float, date.split('-'))
+                self.data[city][year][month][day] = float(temperature)
 
     def getDailyTemperature(self, city, year, month, day):
         '''Returns the temperature for the specified city and date'''
@@ -25,3 +28,12 @@ class TemperatureHelper:
             for each day of the specified year
         '''
         result = np.array(self.data[city][year])
+
+
+data = TemperatureHelper("test1.csv")
+# temp1 = data.getDailyTemperature("Atlanta", 1991, 10, 1)
+
+# myfile = open("test1.csv")
+# lines = myfile.readlines()
+# for line in lines:
+#     print(line)
