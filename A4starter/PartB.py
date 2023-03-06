041d42fd22b524fcede9ff16bd4233d1b4e03615
Carlos Alves [Kuro Suzume] <carlos.ca.eduardo@gmail.com>
2023-03-06 18:14:23 -0300

    Part B completed

--- A4starter/PartB.py
+++ A4starter/PartB.py
@@ -6,18 +6,32 @@ import numpy as np
 class TemperatureHelper:
     def __init__(self,  data_file):
         self.data = {}
-        with open(data_file, "r") as file:
+        lists = []
+        with open(data_file) as file:
             next(file)
             for line in file:
-
-                line.strip("\n")
+                line = line.strip("\n")
                 # split the line
                 lineSplit = line.split(",")
+                # print(lineSplit[0])
                 # for the first line in the file
-
                 city, date, temperature = lineSplit
-                year, month, day = map(float, date.split('-'))
-                self.data[city][year][month][day] = float(temperature)
+                # print(city, temperature)
+                if date == "DATE":
+                    continue
+                # print(date)
+                year, month, day = date.split('-')
+                # create dictionaries for each step if they don't exist
+                if city not in self.data:
+                    self.data[city] = {}
+                if year not in self.data[city]:
+                    self.data[city][year] = {}
+                if month not in self.data[city][year]:
+                    self.data[city][year][month] = {}
+
+                # add temperature data to the day of the month of the year in the city
+                self.data[city][year][month][day] = temperature
+            # print(self.data["Atlanta"]['2011']["01"])
 
     def getDailyTemperature(self, city, year, month, day):
         '''Returns the temperature for the specified city and date'''
@@ -30,10 +44,14 @@ class TemperatureHelper:
         result = np.array(self.data[city][year])
 
 
-data = TemperatureHelper("test1.csv")
-# temp1 = data.getDailyTemperature("Atlanta", 1991, 10, 1)
+# data set goes from 1998 to 2018
 
-# myfile = open("test1.csv")
-# lines = myfile.readlines()
-# for line in lines:
-#     print(line)
+# data = TemperatureHelper("test1.csv")
+# temp1 = data.getDailyTemperature("Atlanta", '2000', '10', '01')
+# print("getDailyTemperature test 1: " + temp1)
+
+# temp2 = data.getDailyTemperature('Fargo', '1998', '05', '21')
+# print("getDailyTemperature test 2: " + temp2)
+
+# temp3 = data.getDailyTemperature('Jacksonville', '2017', '12', '29')
+# print("getDailyTemperature test 3: " + temp3)
